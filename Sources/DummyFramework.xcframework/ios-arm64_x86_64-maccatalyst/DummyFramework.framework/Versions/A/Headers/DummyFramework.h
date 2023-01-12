7dedaff0c770e7ec67d44d3b113c8598ff3b2079
Manuel Alejandro <alejandro>
2023-01-12 13:22:21 -0700

    version 0.0.2

---
+++ Sources/DummyFramework.xcframework/ios-arm64_x86_64-maccatalyst/DummyFramework.framework/Versions/A/Headers/DummyFramework.h
@@ -0,0 +1,18 @@
+//
+//  DummyFramework.h
+//  DummyFramework
+//
+//  Created by Administrador on 12/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyFramework.
+FOUNDATION_EXPORT double DummyFrameworkVersionNumber;
+
+//! Project version string for DummyFramework.
+FOUNDATION_EXPORT const unsigned char DummyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyFramework/PublicHeader.h>
+
+
